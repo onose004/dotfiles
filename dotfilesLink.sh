714dc651dcb22570547a280c68e7b3995af90e34
Ryosuke ONOSE <dev@onose.ws>
2016-05-20 22:42:06 +0900

    modified sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+mkdir -p ~/.vim/bundle
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
 ln -sf ~/dotfiles/bundle ~/.vim
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
