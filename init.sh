e8e06bae8b57c414df081df1139316293c067232
Ryosuke ONOSE <dev@onose.ws>
2016-06-17 17:12:02 +0900

    ubuntu でクリーンインストール可能に

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 # mkdir
-mkdir -p ~/.vim/autoload ~/.vim/bundle 
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 
 # install Oh Mu Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install Colorscheme
 git clone https://github.com/ajh17/Spacegray.vim.git ~/.vim/bundle/Spacegray.vim
+cp ~/.vim/bundle/Spacegray.vim/colors/spacegray.vim ~/.vim/colors/
 
 # install typescript syntax
 git clone https://github.com/leafgarland/typescript-vim.git ~/.vim/bundle/typescript-vim
