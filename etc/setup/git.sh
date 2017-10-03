852e1afea38a917574fa485097866e22d5fde5f7
Ryosuke ONOSE <dev@onose.ws>
2017-10-03 17:50:56 +0900

    modified git.sh

--- etc/setup/git.sh
+++ etc/setup/git.sh
@@ -4,11 +4,26 @@
 # git に関する設定
 #-------------------------------------------------------------------------------
 
+echo Current Settings
+echo user.name=`git config --global user.name`
+echo user.email=`git config --global user.email`
+
+while true; do
+  read -p 'Change settings？[y/n] ' yn
+  case $yn in
+    [Yy] ) break;;
+    [Nn] ) exit 0 ;;
+    * ) echo "Enter [y/n]";;
+  esac
+done
+
 echo "user name: \c"
 read USER_NAME
 git config --global user.name "${USER_NAME}"
 echo "user email: \c"
 read USER_EMAIL
 git config --global user.email "${USER_EMAIL}"
+
+echo Changed Settings
 echo user.name=`git config --global user.name`
 echo user.email=`git config --global user.email`
