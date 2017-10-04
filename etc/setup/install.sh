1173e1115eff70583d4984791adacbab36599ffd
Ryosuke ONOSE <dev@onose.ws>
2017-10-04 17:38:25 +0900

    add xcode-select

---
+++ etc/setup/install.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+xcode-select --install
+
+brew install zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+zsh
+
+brew install lua
+brew reinstall vim --with-lua
