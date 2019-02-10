ecdcb88e4f146be16c28e4392d15d5cab32c5295
Ryosuke Onose <dev@onose.ws>
2019-02-10 20:19:33 +0900

    modify init

--- etc/setup/init.sh
+++ etc/setup/init.sh
@@ -3,15 +3,18 @@
 ## FOR MAC
 
 # xcode-select --install
-# 
-# # prezto
-# zsh
-# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-# setopt EXTENDED_GLOB
-# for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-#   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-# done
-# chsh -s /bin/zsh
+
+# pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+# prezto
+zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
 # 
 # brew install lua
 # brew reinstall vim --with-lua
