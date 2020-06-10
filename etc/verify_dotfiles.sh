1cc7dd7eb6fe1e9d7cdbb558cf5794793dd78bee
Ryosuke Onose <dev@onose.ws>
2020-06-10 14:48:21 +0900

    add verification script

---
+++ etc/verify_dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# requires docker 
+# Note: setup.sh pulls origin/master
+
+docker_images="centos:7 ubuntu:16.04 raspbian/stretch"
+
+for di in $docker_images; do
+  docker run -v $HOME/dotfiles/setup.sh:/setup.sh -it --rm $di /setup.sh || exit 1
+done
+
+"All passed: $docker_images"
+
+exit 0
